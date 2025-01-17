66f1d40867c21766df60e0392589943ee6d540e8
Pedro Damián Caldera Sánchez <pedrodamian150585@gmail.com>
2025-01-17 11:39:16 -0500

    Creando las clases CursoController, DetalleCursoDTO y modificando CursoRepository

--- src/main/java/com/alura/forohub/challengeforohub/domain/curso/dto/DetalleCursoDTO.java
+++ src/main/java/com/alura/forohub/challengeforohub/domain/curso/dto/DetalleCursoDTO.java
@@ -1,4 +1,20 @@
 package com.alura.forohub.challengeforohub.domain.curso.dto;
 
-public record DetalleCursoDTO() {
+import com.alura.forohub.challengeforohub.domain.curso.Categoria;
+import com.alura.forohub.challengeforohub.domain.curso.Curso;
+
+public record DetalleCursoDTO(
+        Long id,
+        String name,
+        Categoria categoria,
+        Boolean activo) {
+
+    public DetalleCursoDTO(Curso curso){
+        this(
+                curso.getId(),
+                curso.getName(),
+                curso.getCategoria(),
+                curso.getActivo());
+    }
+
 }
--- src/main/java/com/alura/forohub/challengeforohub/domain/curso/repository/RepositoryCurso.java
+++
@@ -1,4 +0,0 @@
-package com.alura.forohub.challengeforohub.domain.curso.repository;
-
-public interface RepositoryCurso {
-}
