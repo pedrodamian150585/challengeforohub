cf979f4126e2660f3a158dcf5ed9da371f562c3c
Pedro Damián Caldera Sánchez <pedrodamian150585@gmail.com>
2025-01-17 10:19:23 -0500

    Iniciando mi proyecto challenge forohub

---
+++ src/main/java/com/alura/forohub/challengeforohub/ChallengeforohubApplication.java
@@ -0,0 +1,13 @@
+package com.alura.forohub.challengeforohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeforohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeforohubApplication.class, args);
+	}
+
+}
